bc2367b3b2a58af4e526215377cd9e6e7dee1e66
Joker Jam <jokerjam@yandex.ru>
2023-11-13 00:16:26 +0300

    empty redirect to index

--- cuisine/recipe/urls.py
+++ cuisine/recipe/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.start),
     path("index/", views.index, name="index")
 ]
--- cuisine/recipe/views.py
+++ cuisine/recipe/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
+
+
+def start(request):
+    response = redirect('index/')
+    return response
