0a0e82bd475a12f3f4fe040916ba0cba06b52b1f
Андрей Сапунов <jokerjam@yandex.ru>
2023-11-13 16:50:31 +0300

    recipe append form and template in construction

--- cuisine/recipe/forms.py
+++ cuisine/recipe/forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-
+from django import forms
 from . import models
 
 
@@ -8,3 +8,15 @@ class UserCreateForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = models.User
         fields = UserCreationForm.Meta.fields + ("email", )
+
+
+class RecipeAdd(forms.Form):
+    """
+    Добавление рецепта.
+    """
+    title = forms.CharField(max_length=150)
+    description = forms.CharField(required=False)
+    ingredients = forms.CharField(required=False)
+    steps = forms.CharField(max_length=500)
+    time_cook = forms.IntegerField(min_value=1)
+    photo = forms.ImageField()
--- cuisine/recipe/views.py
+++ cuisine/recipe/views.py
@@ -6,6 +6,7 @@ from django.views import View
 from django.views.generic import FormView
 
 from . import forms
+from . import models
 
 
 # Create your views here.
@@ -25,6 +26,32 @@ def profile(request):
 
 def register_ok(request):
     return render(request, 'registration/register_ok.html')
+
+
+def recipe_add(request):
+    """
+    Добавление рецепта.
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        form = forms.RecipeAdd(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            description = form.cleaned_data['description']
+            ingredients = form.cleaned_data['ingredients']
+            steps = form.cleaned_data['steps']
+            time_cook = form.cleaned_data['time_cook']
+            photo = form.cleaned_data['photo']
+    else:
+        form = forms.RecipeAdd()
+
+    return render(request, 'recipe_add.html', {'form': form})
+
+
+@login_required
+def recipes(request):
+    return render(request, 'recipes.html')
 
 
 class Register(FormView):
