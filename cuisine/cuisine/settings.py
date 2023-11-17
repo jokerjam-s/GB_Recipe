48455e9cb87534eb8b91da201f4d2fb8c078c35b
Андрей Сапунов <jokerjam@yandex.ru>
2023-11-17 17:45:19 +0300

    deploy to host

--- cuisine/cuisine/settings.py
+++ cuisine/cuisine/settings.py
@@ -22,10 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-84q7ad^g*tj0q#&p7zwh=@xhccu$im)^)diqmw2v6@ny5=74s$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
+    'jokerjam.pythonanywhere.com',
 ]
 
 # Application definition
@@ -75,10 +80,24 @@ WSGI_APPLICATION = "cuisine.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jokerjam$default',
+        'USER': 'jokerjam',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'jokerjam.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode = 'STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
     }
 }
 
