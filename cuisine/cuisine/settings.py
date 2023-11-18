2ffb445048aaeb9c579f9798d71b2a79ed9438e1
Андрей Сапунов <jokerjam@yandex.ru>
2023-11-18 12:51:45 +0300

    prepared

--- cuisine/cuisine/settings.py
+++ cuisine/cuisine/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-84q7ad^g*tj0q#&p7zwh=@xhccu$im)^)diqmw2v6@ny5=74s$"
+# SECRET_KEY = "django"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -140,9 +140,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "assets",
+]
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
